4f4f47b72b9ab6a2d2fc5f45f2e7230968b58de4
Enes Demir <92523630+enesdemrr@users.noreply.github.com>
2022-01-11 00:45:46 +0300

    comment line added

--- home_works/json_parser_page/EnesDemir_JsonParser/src/main/java/json_parser/NumberControl.java
+++ home_works/json_parser_page/EnesDemir_JsonParser/src/main/java/json_parser/NumberControl.java
@@ -7,16 +7,16 @@ public class NumberControl {
     public int control(int start){
         do{
         System.out.print("0 ila 10 araliginda bir sayi giriniz: ");
-        if(scanner.hasNextInt()){ // This checks to see if the next input is a valid **int**
+        if(scanner.hasNextInt()){ // This checks to next input is int
             start = scanner.nextInt();
-            if (start>=0 && start<11)
+            if (start>=0 && start<11)//This checks to number range
                 isInt = true;
         }
         else{
             System.out.print("Gecersiz deger girdiniz.\n");
             scanner.next();
         }
-    }while(isInt == false);
+    }while(isInt == false);// loop until correct value is entered
         return start;
     }
 }
